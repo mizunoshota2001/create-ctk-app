99cd2cf4985623072589de6bf6ec4a6ad38a91d4
mizunoshota2001 <95399151+mizunoshota2001@users.noreply.github.com>
2024-11-29 14:27:33 +0900

    TitleクラスにHelloWorldを表示する機能を追加し、lib.sampleからテキストを取得するように変更

--- components/sample/title.py
+++ components/sample/title.py
@@ -1,11 +1,13 @@
 import customtkinter as ctk
 from components import BasePage
+import lib.sample
 
 
 class Title:
     def __init__(self, page: BasePage):
+        text = lib.sample.getHelloWorld()
         ctk.CTkLabel(
             page.frame,
-            text="SamplePage",
+            text=text,
             font=("Comic Sans MS", 50, "bold")
         ).grid(pady=(0, 20))
---
+++ lib/sample.py
@@ -0,0 +1,2 @@
+def getHelloWorld():
+    return "Hello, World!"
